0cd1f3203783fccd933a34c2fe2ffb392bf222fb
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2024-07-29 16:25:40 -0300

    fix: discover button size

--- sections/Discover.tsx
+++ sections/Discover.tsx
@@ -12,7 +12,7 @@ function Discover({ text, buttonText, buttonLink, videoSrc }: Props) {
         <div style="background-image: url(/image/grid-layers.png); background-size: cover; background-repeat: no-repeat; background-position: center;" className="md:pt-[160px] min-h-[700px] md:min-h-[868px]">
             <div className="container px-4 flex flex-col items-center justify-center">
                 <span className="text-[32px] md:text-[64px] font-semibold mb-12 font-oxanium text-center max-w-[1440px]">{text}</span>
-                <a href={buttonLink}> <span className="cursor-pointer bg-primary-content py-[14px] rounded-lg text-white text-base font-poppins w-full max-w-[220px] text-center">{buttonText}</span></a>
+                <a className="w-full flex justify-center" href={buttonLink}> <span className="cursor-pointer bg-primary-content py-[14px] rounded-lg text-white text-base font-poppins w-full max-w-[220px] text-center">{buttonText}</span></a>
                 {videoSrc && <iframe
                     allowFullScreen
                     src={`${videoSrc}?autoplay=1&mute=1`}
